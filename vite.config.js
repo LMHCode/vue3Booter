8d5a08e0ee019009ede5ad9cbce51ccf67f1703e
liuminghui <minghui.liu@palmpay-inc.com>
2022-09-06 17:48:43 +0800

    save

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import postcsspxtoviewport from 'postcss-px-to-viewport'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -16,7 +15,7 @@ export default defineConfig({
     cors: true, // 允许跨域
     host: '0.0.0.0',
     proxy: {
-      '/api': {
+      '/*/api': {
         target: 'http://www.xxxx.com.cn',
         changeOrigin: true
         // rewrite: (path) => path.replace('/api', '')
