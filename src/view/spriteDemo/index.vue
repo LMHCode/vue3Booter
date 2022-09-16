d227e70f078b4152decfc3ac90acb0e5c01d60c8
liuminghui <minghui.liu@palmpay-inc.com>
2022-09-16 15:53:38 +0800

    save

--- src/view/spriteDemo/index.vue
+++ src/view/spriteDemo/index.vue
@@ -31,18 +31,19 @@ const animateBehavior: AnimateBehaviorItem[] = [
     ani: { x: 162, y: 400, scale: [1, 1], rotate: 360 * 5 },
     transitionConfig: {
       duration: 1000,
-      fill: 'both'
+      fill: 'both',
+      easing: 'ease-out'
     }
   },
   {
-    ani: { x: 18, y: 50, scale: [0.9, 0.9], rotate: 360 * 3 },
+    ani: { x: 18, y: 50, scale: [0.7, 0.7], rotate: 360 * 3 },
     transitionConfig: {
       duration: 1000,
       fill: 'both'
     }
   },
   {
-    ani: { x: 50, y: 25, scale: [0.8, 0.8], rotate: 360 * 2 },
+    ani: { x: 50, y: 25, scale: [0.7, 0.7], rotate: 360 * 2 },
     transitionConfig: {
       duration: 1000,
       fill: 'both'
@@ -52,8 +53,7 @@ const animateBehavior: AnimateBehaviorItem[] = [
     ani: { x: 60, y: 45, scale: [0.7, 0.7], rotate: 360 * 1 },
     transitionConfig: {
       duration: 1000,
-      fill: 'both',
-      easing: 'ease-out'
+      fill: 'both'
     }
   }
 ]
@@ -63,15 +63,21 @@ onMounted(() => {
   const scene = new Scene({ container, width: 360, height: 800, mode: 'stickyTop' })
   const layer = scene.layer()
   const robot = new Sprite('https://p5.ssl.qhimg.com/t01c33383c0e168c3c4.png')
+  /**
+   * 重置球初始位置
+   */
   const resetPos = () => {
     robot.attr({
       anchor: [0.5, 0.5], // 锚点，你可以理解为css3中的transform-origin
-      pos: [180, 400], // 位置，最大为 360 * 800 也就是场景的大小
+      pos: [180, 500], // 位置，最大为 360 * 800 也就是场景的大小
       size: [36, 50], // 大小，默认为图片的宽高
       scale: [1, 1] // 缩放，默认 1 1
     })
   }
   resetPos()
+  /**
+   * 踢球
+   */
   const shoot = async () => {
     robot.animate([animateBehavior[0].ani], animateBehavior[0].transitionConfig).finished
     await robot.animate([animateBehavior[1].ani], animateBehavior[1].transitionConfig).finished
